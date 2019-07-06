c9ac9547078997a790da3bbc0f527202fa50eaba
Joshua Crum <joshua.crum@gmail.com>
2019-07-06 20:02:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,60 +13,56 @@ class ApplicationController < Sinatra::Base
 	end
 
 	get "/signup" do
-		erb :signup
-	end
+    erb :signup
+  end
 
-	post "/signup" do
-		#your code here!
-		@user = User.find_by(username: params[:username])
+  post "/signup" do
+    if params[:username] == "" || params[:password] == ""
+      redirect '/failure'
+    else
+      User.create(username: params[:username], password: params[:password])
+      redirect '/login'
+    end
+
+  end
+
+  get '/account' do
+    @user = User.find(session[:user_id])
+    erb :account
+  end
+
+
+  get "/login" do
+    erb :login
+  end
+
+  post "/login" do
+    @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect to "/account"
     else
       redirect to "/failure"
     end
-	end
+  end
 
-	get "/login" do
-		erb :login
-	end
+  get "/failure" do
+    erb :failure
+  end
 
-	post "/login" do
-		#your code here!
-		@user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect to "/account"
-    else
-      redirect to "/failure"
+  get "/logout" do
+    session.clear
+    redirect "/"
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
     end
-	end
 
-	get "/success" do
-		if logged_in?
-			erb :success
-		else
-			redirect "/login"
-		end
-	end
-
-	get "/failure" do
-		erb :failure
-	end
-
-	get "/logout" do
-		session.clear
-		redirect "/"
-	end
-
-	helpers do
-		def logged_in?
-			!!session[:user_id]
-		end
-
-		def current_user
-			User.find(session[:user_id])
-		end
-	end
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 
 end
